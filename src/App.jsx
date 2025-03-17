e6360d5a67046e3a0ab5adafce49a74c2e01f1b6
bata_butter <113527212+batabutter@users.noreply.github.com>
2025-03-17 18:06:26 -0400

    Added new buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './styles.css'
 import { pdfjs } from 'react-pdf';
 import PdfComp from './PdfComp';
+import { LectureDisplay } from './LectureDisplay';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -18,8 +19,20 @@ function App() {
       <h1 className = "title">Collection of UMD Notes : </h1>
 
       <div>
-        <button className = "btnStyle">CMSC 216</button>
-        <label className = "subCategory" >Lecture 1:</label>
+        <div className = "btnFormat">
+          <button className = "btnStyle">CMSC 216</button>
+          <button className = "btnStyle">CMSC 250</button>
+          <button className = "btnStyle">MATH 241</button>
+        </div>
+
+        <div>
+          <button className="dropBtn">Lecture #</button>
+            <div className = "dropContent">
+              <a href="">Lecture 1</a>
+              <a href="">Lecture 2</a>
+            </div>
+        </div>
+
         <PdfComp />
       </div>
     </>
