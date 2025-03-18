a4ec3e99869c456b311043c2cd9f8ac20a3f097d
bata_butter <113527212+batabutter@users.noreply.github.com>
2025-03-18 04:03:32 -0400

    Fixed position of lecture button and added dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -14,27 +14,32 @@ function App() {
 
   return (
     <>
-      <title>UMD Notes App</title>
-
-      <h1 className = "title">Collection of UMD Notes : </h1>
-
-      <div>
-        <div className = "btnFormat">
-          <button className = "btnStyle">CMSC 216</button>
-          <button className = "btnStyle">CMSC 250</button>
-          <button className = "btnStyle">MATH 241</button>
+      <div className="container">
+        
+        <div className="dropdown">
+          <button>Lecture #</button>
+          <div className="dropContent">
+            <a href="">Lecture 1</a>
+            <a href="">Lecture 2</a>
+          </div>
         </div>
 
         <div>
-          <button className="dropBtn">Lecture #</button>
-            <div className = "dropContent">
-              <a href="">Lecture 1</a>
-              <a href="">Lecture 2</a>
+          <h1 className="title">Collection of UMD Notes : </h1>
+
+          <div>
+            <div className="btnFormat">
+              <button className="btnStyle">CMSC 216</button>
+              <button className="btnStyle">CMSC 250</button>
+              <button className="btnStyle">MATH 241</button>
             </div>
+          </div>
+
+          <PdfComp />
         </div>
 
-        <PdfComp />
       </div>
+
     </>
   )
 }
